0ff4ccca1d36a341d674d2a0aabffdc82becd030
Niyitanga Moise <niyitangamoise3@gmail.com>
2021-09-29 05:49:46 -0700

    first summary
    first desc

---
+++ cds/dashboard/dashboard.php
@@ -0,0 +1,118 @@
+<html lang="en">
+    
+        <head>
+            
+        <meta charset="UTF-8">
+         <title>CDS</title>
+         <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-
+         awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+         <link rel="stylesheet" href="style.css ">
+        </head>
+        <body>
+            <div class="sidebar">
+                <div class="sidebar-brand">
+                    <h2><span class="lab la-accusoft"></span>C D S</h2>
+                    
+                </div>
+                <div class="sidebar-menu">
+                    <ul>
+                        <li>
+                            <a href="" class="active"><span class="las la-igloo"></span>
+                        <span>dashboard</span></a>
+                        </li>
+                        <li>
+                            <a href=""><span class="las la-users"></span>
+                        <span>customer</span></a>
+                        </li>
+                        <li>
+                            <a href=""><span class="las la-clipboard-list"></span>
+                        <span>Projects</span></a>
+                        </li>
+                        <li>
+                            <a href=""><span class="las la-shopping-bag"></span>
+                        <span>order</span></a>
+                        </li>
+                        <li>
+                            <a href=""><span class="las la-receipt"></span>
+                        <span>inventory</span></a>
+                        </li>
+                        <li>
+                            <a href=""><span class="las la-user-circle"></span>
+                        <span>Accounts</span></a>
+                        </li> <li>
+                            <a href=""><span class="las la-clipboard-list"></span>
+                        <span>Tasks</span></a>
+                        </li>
+                    </ul>
+                    
+                </div>
+                
+            </div>
+            <div class="main-content">
+                <header>
+                    <div class="header-title">
+                        <h2>
+                        <label for="">
+                            <span class="las la-bars"></span>
+                        </label>
+                        Dashboard
+                        </h2>
+                        <div class="search-wrapper">
+                            <span class="las la-search"></span>
+                            <input type="search" placeholder="search here"/>
+
+                        </div>
+                        <div class="user-wrapper">
+                            <img src="logo.jpg" width="40px" height="40px" alt="">
+                              <div>
+                                  <h4>Moses  </h4>
+                                  <small>Super admin</small>
+                              </div>
+                        </div>
+                    </div>
+                </header>
+                <main>
+                    <div class="cards">
+                        <div class="card-single">
+                            <div>
+                                <h1>54</h1>
+                                <span>Customers</span>
+                            </div>
+                            <div>
+                                <span class="las la-users"></span>
+                            </div>
+                        </div>
+                        <div class="card-single">
+                            <div>
+                                <h1>79</h1>
+                                <span>Project</span>
+                            </div>
+                            <div>
+                                <span class="las la-clipboard-list"></span>
+                            </div>
+                        </div>
+                        <div class="card-single">
+                            <div>
+                                <h1>124</h1>
+                                <span>Orders</span>
+                            </div>
+                            <div>
+                                <span class="las la-shopping-bag"></span>
+                            </div>
+                        </div>
+                        <div class="card-single">
+                            <div>
+                                <h1>$5k</h1>
+                                <span>Income</span>
+                            </div>
+                            <div>
+                                <span class="las la-google-wallet"></span>
+                            </div>
+                        </div>
+
+                    </div>
+                </main>
+            </div>
+        </body>
+    
+</html>
---
+++ cds/index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+	<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <meta http_equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css" media="screen" href="anmtn/back_style.css" />
+		
+        <link rel="stylesheet" href="styles.css">
+        <title>Login -CDS</title>
+	</head>
+	<body class="login-background">
+        <div class="slider">
+        <div class="load">
+            
+
+        <div id="navbar">
+   
+   
+
+    <h1>Class Diary System</h1>
+    
+
+
+
+		<?php include('Common/common-header.php')?>
+        <div class="login-div mt-3 rounded">
+            <div class="logo-div text-center">
+                <img src="images/lock.png" alt="" class="align-items-center" width="100" height="100">
+            </div>
+            <div class="login-padding">
+                <h2 class="text-center text-white">LOGIN</h2>
+                <form class="p-1" action="login.php" method="POST">
+                    <div class="form-group">
+                        <label><h6>Enter Your Email:</h6></label>
+                        <input type="text" name="email" placeholder="Enter User ID" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label><h6>Enter Your Password:</h6></label>
+                        <input type="Password" name="password" placeholder="Enter Password" class="form-control border-bottom" required>
+                       
+                    </div>
+                    <div class="form-group text-center mb-3 mt-4">
+                    <div class="form-group mt-20">
+                                                    		<div class="col-sm-offset-2 col-sm-10">
+                                                           
+                                                    			<button type="submit" name="login" class="btn btn-success btn-labeled pull-right">Sign in<span class="btn-label btn-label-right"><i class="fa fa-check"></i></span></button>
+                                                    		</div>
+                                                    	</div>
+                    </div></div>
+                </form></div></<div>
+            
+                </div>
+            </div></div>
+        </div>
+    </body>
+</html>
+
+
+
